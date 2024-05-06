0127f7f6e460c04a3f4d7a27bc2bc0f0bd450e67
EvanaPerveenEva <evana.perveen@welldev.io>
2024-05-06 17:32:08 +0600

    models, schemas and configs setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
+import logging
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
+
 
 from app.core.config import cors_middleware
 from app.core.database import create_all_tables
 
-from app.api.v1.routes import routers as v1_routers
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 load_dotenv()
 
@@ -18,11 +23,11 @@ app.add_middleware(cors_middleware)
 @app.on_event("startup")
 async def startup_event():
     await create_all_tables()
-    
-    
+
+
 @app.get("/")
 def root():
     return "To-Do is working"
-    
-app.include_router(v1_routers, prefix="/api/v1")
+
+
 
